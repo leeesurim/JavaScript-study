8056bb30c7d534d50fc4ee92662139fe818e6529
leeesurim <108252916+leeesurim@users.noreply.github.com>
2022-11-23 18:33:42 +0900

    study: function

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+        console.log("Hello! " + otherPersonsName + " nict to meet you!");
+    }
+};
+
+console.log(player.name);
+player.sayHello("toby");
