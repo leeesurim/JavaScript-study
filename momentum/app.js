65c98dd448b8bc5df908e67bab8a87f7eebed40f
leesurim <12esuq4@gmail.com>
2022-11-27 01:16:02 +0900

    study: event

--- momentum/app.js
+++ momentum/app.js
@@ -1,20 +1,20 @@
-const body = document.getElementById("bg");
-var intViewportWidth = window.innerWidth;
+const body = document.body;
+const BIG_SCREEN = "bigScreen";
+const MEDIUM_SCREEN = "mediumScreen";
+const SMALL_SCREEN = "smallScreen";
+const width = window.innerWidth;
+document.querySelectorAll
 
 function handleWindowResize(){
-    if(intViewportWidth >= 1200){
-        body.classList.remove("bg");
-        body.classList.remove("minus");
-        body.classList.add("plus");
-    }else if(intViewportWidth < 1200 && intViewportWidth >=800){
-        body.classList.remove("plus");
-        body.classList.remove("minus");
-        body.classList.add("bg");
-        
-    }else if(intViewportWidth < 800 && intViewportWidth >= 400){
-        body.classList.remove("plus");
-        body.classList.remove("bg");
-        body.classList.add("minus");
+    if(width >= 1000){
+        body.classList.add(BIG_SCREEN);
+        body.classList.remove(MEDIUM_SCREEN);
+    }else if(width <= 1140 && width >=700){
+        body.classList.add(MEDIUM_SCREEN);
+        body.classList.remove(BIG_SCREEN);
+    }else {
+        body.classList.remove(MEDIUM_SCREEN);
+        body.classList.add(SMALL_SCREEN);
     }
 
 }
