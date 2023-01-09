db51472daefa20d26852e6e27e3eda242f96f02e
leesurim <12esuq4@gmail.com>
2023-01-09 18:19:42 +0900

    study: js

--- javascript-for-beginners/app.js
+++ javascript-for-beginners/app.js
@@ -1,2 +1,6 @@
-const title = document.getElementById("h1");
-// 5:15 / 12:24
+// querySelector : element를 CSS selector방식으로 검색할 수 있음
+// querySelector - 첫번째 element만 가져옴
+// querySelectorAll : selector 안의 조건에 부합하는 모든 element 가져옴
+const title = document.querySelector(".hello h1:first-child");
+
+console.log(title);
